a5866d53d70098359a5ddc6dcdbaa27ab8ae8152
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-06-05 20:07:10 -0400

    Implements filtered component for the controlled search functionality

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,33 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
+
+  function handleSearchChange(event) {
+    setSearch(event.target.value);
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
+    const matchesCategory =
+      selectedCategory === "All" || item.category === selectedCategory;
+    const matchesSearch = item.name
+      .toLowerCase()
+      .includes(search.toLowerCase());
+    return matchesCategory && matchesSearch;
   });
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter
+        onCategoryChange={handleCategoryChange}
+        search={search}
+        onSearchChange={handleSearchChange}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
